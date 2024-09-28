7cdd24e2f90b89b41795d44ba4b8d7d0da13c0c7
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-09-28 15:53:28 +0300

    Start solve task E

---
+++ 3_semester/contest1/e-task.cpp
@@ -0,0 +1,24 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+
+    std::cin >> n >> m;
+
+    std::vector<int> coords(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> coords[i];
+    }
+
+    int 
+
+    return 0;
+}
