175b59938f7b7c2f4e2ab95df0922dac66ef865b
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-10-07 22:18:57 +0300

    Final solution of problem C

--- 3_semester/contest2/c-task.cpp
+++ 3_semester/contest2/c-task.cpp
@@ -7,7 +7,7 @@
 static unsigned int INF = (std::numeric_limits<unsigned int>::max() / 4);
 
 struct HamiltonianPath {
-  unsigned int              min_cost;
+  unsigned int min_cost;
   std::vector<unsigned int> vertex_sequence;
 };
 
@@ -16,37 +16,27 @@ bool IsVertexInMask(unsigned int bit_mask, unsigned int vertex) {
   return ((bit_mask & vertex_mask) == vertex_mask) ? true : false;
 }
 
-HamiltonianPath FindHamiltonianPathWithMinCost(std::vector<std::vector<unsigned int>>& graph) {
+HamiltonianPath FindHamiltonianPathWithMinCost(
+    std::vector<std::vector<unsigned int>>& graph) {
   size_t max_bit_mask = (1 << graph.size()) - 1;
 
-  std::vector<std::vector<unsigned int>> path_cost(graph.size(), std::vector<unsigned int>(max_bit_mask + 1, INF));
-  std::vector<std::vector<unsigned int>> prev     (graph.size(), std::vector<unsigned int>(max_bit_mask + 1, INF));
+  std::vector<std::vector<unsigned int>> path_cost(
+      graph.size(), std::vector<unsigned int>(max_bit_mask + 1, INF));
+  std::vector<std::vector<unsigned int>> prev(
+      graph.size(), std::vector<unsigned int>(max_bit_mask + 1, INF));
 
   for (std::size_t i = 0; i < graph.size(); ++i) {
-    path_cost[i][1u << i] = 0; 
+    path_cost[i][1u << i] = 0;
   }
-
-  /*std::cout << "==============\n";
-  for (unsigned int vertex = 0; vertex < graph.size(); ++vertex) {
-    for (unsigned int bit_mask = 0; bit_mask <= max_bit_mask; ++bit_mask) {
-        std::cout << "path_cost[" << vertex << "][" << bit_mask << "] := " << path_cost[vertex][bit_mask] << '\n';
-    }
-    std::cout << "-------------\n";
-  }
-  std::cout << "==============\n\n";*/
 
   for (unsigned int bit_mask = 1; bit_mask <= max_bit_mask; ++bit_mask) {
     for (std::size_t v = 0; v < graph.size(); ++v) {
       if (IsVertexInMask(bit_mask, v)) {
-        //std::cout << "bitmask := " << bit_mask << " | v := " << v << '\n';
         for (std::size_t u = 0; u < graph.size(); ++u) {
           if (!IsVertexInMask(bit_mask, u)) {
-            //std::cout << "u := " << u << '\n';
             unsigned int& target = path_cost[u][bit_mask | (1u << u)];
-            //std::cout << path_cost[u][bit_mask | (1u << u)] << '\n';
             if (target > (path_cost[v][bit_mask] + graph[v][u])) {
               target = path_cost[v][bit_mask] + graph[v][u];
-              //std::cout << "new: " << path_cost[v][bit_mask] + graph[v][u] << '\n';
               prev[u][bit_mask | (1u << u)] = v;
             }
           }
@@ -55,21 +45,12 @@ HamiltonianPath FindHamiltonianPathWithMinCost(std::vector<std::vector<unsigned 
     }
   }
 
-  /*std::cout << "==============\n";
-  for (unsigned int vertex = 0; vertex < graph.size(); ++vertex) {
-    for (unsigned int bit_mask = 0; bit_mask <= max_bit_mask; ++bit_mask) {
-        std::cout << "path_cost[" << vertex << "][" << bit_mask << "] := " << path_cost[vertex][bit_mask] << '\n';
-    }
-    std::cout << "-------------\n";
-  }
-  std::cout << "==============\n\n";*/
-
-  unsigned int min_cost     = INF;
+  unsigned int min_cost = INF;
   unsigned int final_vertex = INF;
 
   for (unsigned int vertex = 0; vertex < graph.size(); ++vertex) {
     if (min_cost > path_cost[vertex][max_bit_mask]) {
-      min_cost     = path_cost[vertex][max_bit_mask];
+      min_cost = path_cost[vertex][max_bit_mask];
       final_vertex = vertex;
     }
   }
@@ -78,13 +59,15 @@ HamiltonianPath FindHamiltonianPathWithMinCost(std::vector<std::vector<unsigned 
   result.min_cost = min_cost;
 
   unsigned int cur_bit_mask = max_bit_mask;
-  unsigned int cur_vertex   = final_vertex;
+  unsigned int cur_vertex = final_vertex;
 
   do {
     result.vertex_sequence.push_back(cur_vertex + 1);
     cur_bit_mask = cur_bit_mask - (1u << cur_vertex);
     cur_vertex = prev[cur_vertex][cur_bit_mask | (1u << cur_vertex)];
   } while (cur_bit_mask != 0);
+
+  std::reverse(result.vertex_sequence.begin(), result.vertex_sequence.end());
 
   return result;
 }
@@ -93,7 +76,8 @@ int main() {
   unsigned int vertex_num = 0;
   std::cin >> vertex_num;
 
-  std::vector<std::vector<unsigned int>> graph(vertex_num, std::vector<unsigned int>(vertex_num, 0));
+  std::vector<std::vector<unsigned int>> graph(
+      vertex_num, std::vector<unsigned int>(vertex_num, 0));
 
   for (unsigned int i = 0; i < vertex_num; ++i) {
     for (unsigned int j = 0; j < vertex_num; ++j) {
@@ -109,6 +93,6 @@ int main() {
     std::cout << vertex << ' ';
   }
   std::cout << '\n';
-  
+
   return 0;
 }
