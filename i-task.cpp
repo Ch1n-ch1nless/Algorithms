11f4535bf74ef777c6a70ddcb6621119752cd8af
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-11-10 12:22:44 +0300

    Made edits

--- i-task.cpp
+++ i-task.cpp
@@ -2,19 +2,49 @@
 #include <iostream>
 #include <vector>
 
-static int LOW = 0;   // sign '<'
-static int GREAT = 1; // sign '>'
+static int LOW = 0;    // sign '<'
+static int GREAT = 1;  // sign '>'
 
-std::vector<int>
-FindLargestAlternatingSequence(const std::vector<int> &src_seq) {
+std::vector<int> RestoreSequence(
+    const std::vector<int> &src_seq,
+    const std::vector<std::vector<int>> &alt_seq_len, int max_len, int sign,
+    int pos) {
+  std::vector<int> res_seq = {};
+
+  res_seq.push_back(src_seq[pos]);
+
+  int cur_elem = src_seq[pos];
+
+  while (max_len > 1) {
+    --max_len;
+    sign = 1 - sign;  // Change the sign
+    --pos;
+
+    while (alt_seq_len[pos][sign] != max_len ||
+           !((sign == LOW) ? std::less(src_seq[pos], cur_elem)
+                           : std::greater(src_seq[pos], cur_elem))) {
+      --pos;
+    }
+
+    res_seq.push_back(src_seq[pos]);
+    cur_elem = src_seq[pos];
+  }
+
+  std::reverse(res_seq.begin(), res_seq.end());
+
+  return res_seq;
+}
+
+std::vector<int> FindLargestAlternatingSequence(
+    const std::vector<int> &src_seq) {
   int n = src_seq.size();
 
-  //  alt_seq_len - vector, which contains max len of alternating sequence, 
-  //which ends on some index of src_seq. 
-  //  alt_seq_len[i][LOW]   := max len of alternating sequence, 
-  //which ends on index i & exists j < i, that a[j] < a[i] 
-  //  alt_seq_len[i][GREAT] := max len of alternating sequence, 
-  //which ends on index i & exists j < i, that a[j] > a[i]
+  //  alt_seq_len - vector, which contains max len of alternating sequence,
+  // which ends on some index of src_seq.
+  //  alt_seq_len[i][LOW]   := max len of alternating sequence,
+  // which ends on index i & exists j < i, that a[j] < a[i]
+  //  alt_seq_len[i][GREAT] := max len of alternating sequence,
+  // which ends on index i & exists j < i, that a[j] > a[i]
   std::vector<std::vector<int>> alt_seq_len(n, std::vector<int>(2, 0));
 
   alt_seq_len[0][LOW] = 1;
@@ -49,62 +79,38 @@ FindLargestAlternatingSequence(const std::vector<int> &src_seq) {
     }
   }
 
-  std::vector<int> res_seq = {};
-
-  res_seq.push_back(src_seq[pos]);
-
-  int cur_elem = src_seq[pos];
-
-  while (max_len > 1) {
-    --max_len;
-    sign = 1 - sign; // Change the sign
-    --pos;
-
-    while (alt_seq_len[pos][sign] != max_len ||
-           !((sign == LOW) ? std::less(src_seq[pos], cur_elem)
-                           : std::greater(src_seq[pos], cur_elem))) {
-      --pos;
-    }
-
-    res_seq.push_back(src_seq[pos]);
-    cur_elem = src_seq[pos];
-  }
-
-  std::reverse(res_seq.begin(), res_seq.end());
-
-  return res_seq;
+  return RestoreSequence(src_seq, alt_seq_len, max_len, sign, pos);
 }
 
-void ReadVector(std::vector<int> &vec, std::size_t vec_len) {
+std::vector<int> ReadVector(std::size_t vec_len) {
   int temp_var = 0;
+  std::vector<int> vec(vec_len);
 
   for (std::size_t i = 0; i < vec_len; ++i) {
-    std::cin >> temp_var;
-    vec.push_back(temp_var);
+    std::cin >> vec[i];
   }
+
+  return vec;
 }
 
 void PrintVector(const std::vector<int> &vec) {
-  std::cout << vec.size() << '\n';
-
   for (std::size_t i = 0; i < vec.size(); ++i) {
     std::cout << vec[i] << ' ';
   }
-  std::cout << '\n';
 }
 
 int main() {
   std::size_t number_of_regions = 0;
   std::cin >> number_of_regions;
 
-  std::vector<int> gangster_sequence;
-
-  ReadVector(gangster_sequence, number_of_regions);
+  std::vector<int> gangster_sequence = ReadVector(number_of_regions);
 
   std::vector<int> answer_sequence =
       FindLargestAlternatingSequence(gangster_sequence);
 
+  std::cout << answer_sequence.size() << '\n';
   PrintVector(answer_sequence);
+  std::cout << '\n';
 
   return 0;
 }
