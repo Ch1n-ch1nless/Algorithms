856e8a5afb3bbf4ec32d83ce7c3a3b4dc4ee894d
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-11-30 22:58:05 +0300

    Solve problem task E from contest #5

--- 3_semester/contest5/e-task.cpp
+++ 3_semester/contest5/e-task.cpp
@@ -10,12 +10,14 @@ struct Edge {
 
 using Graph = std::vector<Edge>;
 
-const int INF = __INT_MAX__;
+const int INF = 100000;
 
 class NegativeWeightCycleDetector {
  public:
   NegativeWeightCycleDetector(const Graph& graph, size_t vertex_number);
   ~NegativeWeightCycleDetector() = default;
+
+  bool isNegativeCycleDetected() const;
 
   std::vector<int>& getVertexListInCycle();
   const std::vector<int>& getVertexListInCycle() const;
@@ -24,15 +26,17 @@ class NegativeWeightCycleDetector {
   void findNegativeWeightCycleByBellmanFord(const Graph& graph,
                                             size_t vertex_number);
 
-  bool is_graph_contain_neg_cycle_;
-  int  cycle_start_vertex_;
-  std::vector<int> vertex_list_;
+  void restoreVertexCycle(std::vector<int>& parent);
+
+  bool is_graph_contains_neg_cycle_ = false;
+  int cycle_start_vertex_ = -1;
+  std::vector<int> vertex_cycle_;
 };
 
 void NegativeWeightCycleDetector::findNegativeWeightCycleByBellmanFord(
     const Graph& graph, size_t vertex_number) {
   size_t try_number = 0;
-  bool   is_updated = true;
+  bool is_updated = true;
 
   std::vector<int> distances(vertex_number, INF);
   std::vector<int> parent(vertex_number, -1);
@@ -41,26 +45,101 @@ void NegativeWeightCycleDetector::findNegativeWeightCycleByBellmanFord(
     is_updated = false;
 
     for (auto cur_edge : graph) {
-      int start  = cur_edge.start;
+      int start = cur_edge.start;
       int finish = cur_edge.finish;
-      int cost   = cur_edge.cost;
+      int cost = cur_edge.cost;
 
-      if (distances[start] != INF && (distances[start] + cost < distances[finish]))
-      {
-          distances[finish] = distances[start] + cost;
-          parent[finish] = start;
-          is_updated = true;
+      if ((distances[start] + cost) < distances[finish]) {
+        distances[finish] = distances[start] + cost;
+        parent[finish] = start;
+        is_updated = true;
 
-          if (try_number == vertex_number) {
-            cycle_start_vertex_ = start;
-            break;
-          }
+        if (try_number == vertex_number) {
+          cycle_start_vertex_ = start;
+          break;
+        }
       }
-      try_number++;
-    }   
+    }
+    try_number++;
   }
 
   if (is_updated) {
-    
+    is_graph_contains_neg_cycle_ = true;
+    restoreVertexCycle(parent);
+    return;
   }
+
+  is_graph_contains_neg_cycle_ = false;
+  return;
+}
+
+void NegativeWeightCycleDetector::restoreVertexCycle(std::vector<int>& parent) {
+  int v = cycle_start_vertex_;
+  for (size_t i = 0; i < parent.size(); i++) {
+    v = parent[v];
+  }
+  int u = v;
+  v = parent[v];
+  while (u != v) {
+    vertex_cycle_.push_back(v);
+    v = parent[v];
+  }
+  vertex_cycle_.push_back(v);
+  vertex_cycle_.push_back(parent[v]);
+
+  std::reverse(vertex_cycle_.begin(), vertex_cycle_.end());
+}
+
+NegativeWeightCycleDetector::NegativeWeightCycleDetector(const Graph& graph,
+                                                         size_t vertex_number)
+    : is_graph_contains_neg_cycle_(false),
+      cycle_start_vertex_(-1),
+      vertex_cycle_() {
+  findNegativeWeightCycleByBellmanFord(graph, vertex_number);
+}
+
+bool NegativeWeightCycleDetector::isNegativeCycleDetected() const {
+  return is_graph_contains_neg_cycle_;
+}
+
+std::vector<int>& NegativeWeightCycleDetector::getVertexListInCycle() {
+  return vertex_cycle_;
+}
+
+const std::vector<int>& NegativeWeightCycleDetector::getVertexListInCycle()
+    const {
+  return vertex_cycle_;
+}
+
+int main() {
+  int vertex_number = 0;
+  std::cin >> vertex_number;
+
+  Graph graph;
+
+  for (int start_vertex = 0; start_vertex < vertex_number; start_vertex++) {
+    for (int end_vertex = 0; end_vertex < vertex_number; end_vertex++) {
+      int weight = 0;
+      std::cin >> weight;
+      if (weight != INF)
+        graph.push_back(Edge{start_vertex, end_vertex, weight});
+    }
+  }
+
+  NegativeWeightCycleDetector cycle_detector(graph, vertex_number);
+
+  if (cycle_detector.isNegativeCycleDetected() == false) {
+    std::cout << "NO\n";
+  } else {
+    std::cout << "YES\n";
+    std::vector<int> vertex_negative_cycle =
+        cycle_detector.getVertexListInCycle();
+    std::cout << vertex_negative_cycle.size() << '\n';
+    for (auto vertex : vertex_negative_cycle) {
+      std::cout << vertex + 1 << ' ';
+    }
+    std::cout << '\n';
+  }
+
+  return 0;
 }
