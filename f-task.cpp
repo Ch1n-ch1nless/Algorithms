86888c3a7dc97c98e7c61753ee777d4b4cfc5117
Ch1n-ch1nless <aleksandrov.oa@phystech.edu>
2024-12-07 13:38:04 +0300

    Implement class Graph

--- f-task.cpp
+++ f-task.cpp
@@ -2,18 +2,87 @@
 #include <iostream>
 #include <vector>
 
-using Graph = std::vector<std::vector<int>>;
+using Vertex = int;
+
+class Graph {
+public:
+  Graph() = default;
+  Graph(size_t vertex_number);
+  Graph(const Graph& other);
+
+  ~Graph() = default;
+
+  Graph& operator=(const Graph& other);
+
+  std::vector<bool>& operator[](const Vertex& vertex);
+  const std::vector<bool>& operator[](const Vertex& vertex) const;
+
+  size_t getVertexNum() const;
+  void   setVertexNum(size_t new_vertex_number);
+
+  void addEdge(const Vertex& start, const Vertex& end);
+  bool isEdgeExists(const Vertex& start, const Vertex& end) const;
+
+private:
+  std::vector<std::vector<bool>> adjacency_matrix_;
+};
+
+Graph::Graph(size_t vertex_number) 
+:
+  adjacency_matrix_(vertex_number, std::vector<bool>(vertex_number, false))
+{
+}
+
+Graph::Graph(const Graph& other)
+:
+  adjacency_matrix_(other.adjacency_matrix_)
+{
+}
+
+Graph& Graph::operator=(const Graph& other) {
+  if (this != &other) {
+    adjacency_matrix_ = other.adjacency_matrix_;
+  }
+  return *this;
+}
+
+std::vector<bool>& Graph::operator[](const Vertex& vertex) {
+  return adjacency_matrix_[vertex];
+}
+
+const std::vector<bool>& Graph::operator[](const Vertex& vertex) const {
+  return adjacency_matrix_[vertex];
+}
+
+size_t Graph::getVertexNum() const {
+  return adjacency_matrix_.size();
+}
+
+void Graph::setVertexNum(size_t new_vertex_number) {
+  adjacency_matrix_.resize(new_vertex_number);
+  for (auto iter = adjacency_matrix_.begin(); iter != adjacency_matrix_.end(); ++iter) {
+    iter->resize(new_vertex_number);
+  }
+}
+
+void Graph::addEdge(const Vertex& start, const Vertex& end) {
+  adjacency_matrix_[start][end] = true;
+}
+
+bool Graph::isEdgeExists(const Vertex& start, const Vertex& end) const {
+  return adjacency_matrix_[start][end];
+}
 
 Graph findTransitiveClosureMatrixOfSrcGraph(const Graph& src_graph) {
-  size_t vertex_number = src_graph.size();
+  size_t vertex_number = src_graph.getVertexNum();
 
   Graph trans_closure_graph = src_graph;
 
   for (size_t k = 0; k < vertex_number; ++k) {
     for (size_t i = 0; i < vertex_number; ++i) {
       for (size_t j = 0; j < vertex_number; ++j) {
-        if (trans_closure_graph[i][k] == 1 && trans_closure_graph[k][j] == 1) {
-          trans_closure_graph[i][j] = 1;
+        if (trans_closure_graph.isEdgeExists(i, k) && trans_closure_graph.isEdgeExists(k, j)) {
+          trans_closure_graph.addEdge(i, j);
         }
       }
     }
@@ -26,13 +95,13 @@ std::istream& operator>>(std::istream& in, Graph& graph) {
   size_t vertex_number = 0;
   in >> vertex_number;
 
-  graph.resize(vertex_number);
+  bool is_edge_exist = false;
 
+  graph.setVertexNum(vertex_number);
   for (size_t i = 0; i < vertex_number; i++) {
-    graph[i].resize(vertex_number);
-
     for (size_t j = 0; j < vertex_number; j++) {
-      in >> graph[i][j];
+      in >> is_edge_exist;
+      if (is_edge_exist) graph.addEdge(i, j);
     }
   }
 
@@ -40,7 +109,7 @@ std::istream& operator>>(std::istream& in, Graph& graph) {
 }
 
 std::ostream& operator<<(std::ostream& out, const Graph& graph) {
-  for (size_t i = 0; i < graph.size(); ++i) {
+  for (size_t i = 0; i < graph.getVertexNum(); ++i) {
     for (size_t j = 0; j < graph[i].size(); ++j) {
       out << graph[i][j] << ' ';
     }
